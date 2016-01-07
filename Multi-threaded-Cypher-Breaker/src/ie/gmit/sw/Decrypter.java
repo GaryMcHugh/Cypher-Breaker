c535686d40ea0f1b3f42adce0c57ac4667fc4cff
gary <gary@GarysPC>
2016-01-07 20:01:10 +0000

    Added code

---
+++ Multi-threaded-Cypher-Breaker/src/ie/gmit/sw/Decrypter.java
@@ -0,0 +1,30 @@
+package ie.gmit.sw;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Decrypter implements Runnable{
+	private BlockingQueue<Resultable> queue;
+	private String cypherText;
+	private int key;
+	
+	public Decrypter(BlockingQueue<Resultable> queue, String cypherText, int key) {
+		super();
+		this.queue = queue;
+		this.cypherText = cypherText;
+		this.key = key;
+	}
+
+	public void run() {
+		RailFence rf = new RailFence(); //RailFence is a class given
+		String plainText = rf.decrypt(cypherText, key);
+		//get the score
+				
+		Resultable r = null; //create a result
+		try {
+			queue.put(r);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}//surround with a try catch
+	}
+
+}
