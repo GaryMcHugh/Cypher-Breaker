5e8a7a69163e5db091dba5b25beba42042682cd7
gary <gary@GarysPC>
2016-01-10 14:41:34 +0000

    README

--- Multi-threaded-Cypher-Breaker/src/ie/gmit/sw/FileParser.java
+++ Multi-threaded-Cypher-Breaker/src/ie/gmit/sw/FileParser.java
@@ -4,10 +4,14 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.io.*;
 
-public class fileParser {
+public class FileParser {
 	
 	public Map<String, Double> parse(String File) throws IOException{
 		Map<String,Double> temp = new ConcurrentHashMap<String, Double>();
+		
+		String splitString = "";
+		String key;
+		Double text;
 		
 		BufferedReader br= new BufferedReader(new InputStreamReader(new FileInputStream("4grams.txt")));
 		
@@ -15,6 +19,18 @@ public class fileParser {
 		
 		while((next=br.readLine()) != null){
 			//add each 4gram to the hash map
+			
+			// split on the space
+			String[] parseArray = splitString.split(" ");
+			
+			//key is first value in 4grams
+			key = parseArray[0];
+			
+			//text is second value in 4grams
+			text = Double.parseDouble(parseArray[1]);
+					
+			// add values to hashMap
+			temp.put(key,  text);
 		}
 		return null;
 	}
--- Multi-threaded-Cypher-Breaker/src/ie/gmit/sw/Runner.java
+++ Multi-threaded-Cypher-Breaker/src/ie/gmit/sw/Runner.java
@@ -1,14 +1,18 @@
 package ie.gmit.sw;
 
+import java.io.IOException;
 import java.util.*;
 
 public class Runner {
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		
 		Scanner console = new Scanner(System.in);
 		
 		String cypherText;
 		int key;
+		
+		Map<String, Double> cypherMap;
 		
 	    System.out.print("Enter the Railfence Key: ");
 	    key = console.nextInt();
@@ -18,8 +22,19 @@ public class Runner {
 	    System.out.print("Enter the Cypher text: ");
 	    cypherText = console.nextLine();
 	    
-	    //pass to be decrypted and then score it
+	    //I used a key of 3 and cypher message HYSAPDYPA
+	    System.out.println();
+	    System.out.println("Your key is: " + key);
+	    System.out.println("Your message is: " + cypherText);
 	    
+	    //create instances of failFence and fileParser
+		RailFence railF = new RailFence();
+		
+		FileParser fileP = new FileParser();
+	    
+		
+		//parse 4grams
+		cypherMap = fileP.parse("4grams.txt");
 	}
 
 }
